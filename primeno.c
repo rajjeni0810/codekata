63a5d6054bac789414a426bbc37617cffffe7956
rajjeni0810 <47377928+rajjeni0810@users.noreply.github.com>
2019-03-08 11:39:02 +0530

    Rename prime.c to primeno.c

---
+++ primeno.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<math.h>
+int main(void) {
+int x,y;
+scanf("%d %d",&x,&y);
+for(int j=x;j<y;j++)
+{
+int count=0;
+for(int i=2;i<j;i++)
+{
+if(j%i==0)
+count++;
+}
+if(count==0)
+{
+	printf("%d\n",j);
+}
+
+}
+	return 0;
+}
