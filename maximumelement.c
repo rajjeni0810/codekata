16d50b964ca879a47f8ee42b8a7e57747a44a791
rajjeni0810 <47377928+rajjeni0810@users.noreply.github.com>
2019-03-08 13:42:01 +0530

    Update maximumelement.c

--- maximumelement.c
+++ maximumelement.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main()
 {
-	int n,m,i,gcd;
-	scanf("%d %d",&n,&m);
+	int a,b,i,gcd;
+	scanf("%d %d",&a,&b);
 	
-	for(i=2;i<=n&&i<=m;i++)
+	for(i=2;i<=a&&i<=b;i++)
 	{
-              if(n%i==0&&m%i==0)
+              if(a%i==0&&b%i==0)
               gcd=i;
 	}
 	printf("%d %d",gcd,gcd);
