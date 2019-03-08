5f28180f0f2f4a1171c4bbc7ce1cea3b43ac0156
rajjeni0810 <47377928+rajjeni0810@users.noreply.github.com>
2019-03-08 12:01:58 +0530

    Update armstrong.c

--- armstrong.c
+++ armstrong.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-int a,b,i,j,rem;
-scanf("%d %d",&a,&b);
-for(i=a;i<b;i++)
+int m,n,i,j,rem;
+scanf("%d %d",&m,&n);
+for(i=m;i<n;i++)
 {
 int p=i,sum=0;
 for(j=0;p!=0;j++)
