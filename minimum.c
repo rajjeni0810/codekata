cafb20609c53a6b35ecc18347fc5f0ffbf7f05fc
rajjeni0810 <47377928+rajjeni0810@users.noreply.github.com>
2019-03-09 09:53:29 +0530

    Update minimum.c

--- minimum.c
+++ minimum.c
@@ -1,15 +1,18 @@
 #include<stdio.h>
 int main()
 {
-	int a,b,i,gcd,lcm;
-	scanf("%d %d",&a,&b);
-	
-	for(i=2;i<=a&&i<=b;i++)
-	{
-              if(b%i==0&&a%i==0)
-              gcd=i;
-	}
-	lcm=(a*b)/gcd;
-	printf("%d",lcm);
-	return 0;
+    int a,b,i,gcd,lcm,a1,b1;
+    scanf("%d %d",&a,&b);
+    a1=a;
+    b1=b;
+    for(i=1;i<=b1&&i<=a1;i++)
+    {
+        if(((b1%i)==0) && ((a1%i)==0))
+        gcd=i;
+    }
+    
+     lcm=(b1*a1)/gcd;
+     printf("%d",lcm);
+     
+        return 0;
 }
